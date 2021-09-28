a3af9c3896ded0554352fd55fd8101d9714589ba
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-09-28 15:50:12 -0400

    Create transpose.py

---
+++ transpose.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 15:13:00 2021
+
+@author: jt2018
+"""
+
+import pandas as pd
+
+data = {'zip5' :['00000','00001','00002','99998','99999'],
+        'state':['US','US','US','US','US']}     
+
+ 
+hpiindex = pd.DataFrame(data)
+hpiindex
+
