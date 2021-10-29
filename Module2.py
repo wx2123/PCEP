f3634bdee265486ad48a3b00a5a0ac24ed3858e8
David <51027303+wx2123@users.noreply.github.com>
2021-10-29 19:58:04 -0400

    Update Module2.py

--- Module2.py
+++ Module2.py
@@ -10,3 +10,18 @@ Created on Fri Oct 29 19:28:50 2021
 
 # 2.1.1.1 Your very first program
 print("Hello, World!")
+
+
+# 2.1.1.6 LAB: The print() function
+print("Hello, Python!")
+print("David")
+#print(david)
+
+# 2.1.1.8 Your very first program
+print("The itsy bitsy spider climbed up the waterspout.")
+print("Down came the rain and washed the spider out.")
+
+# 2.1.1.9 Your very first program
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.")
