2fc9c052c93e27974cc0948b4a05d104418a902a
David <51027303+wx2123@users.noreply.github.com>
2021-10-30 10:37:25 -0400

    Update Module2.py

--- Module2.py
+++ Module2.py
@@ -31,3 +31,23 @@ print("The itsy bitsy spider\nclimbed up the waterspout.")
 print()
 print("Down came the rain\nand washed the spider out.")
 
+# 2.1.1.11 Your very first program
+# print("\")
+print("\\")
+
+# 2.1.1.12 Your very first program: The print() function - using multiple arguments
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+
+# 2.1.1.13 Your very first program: The print() function - the positional way of passing the arguments
+print("My name is", "Python.")
+print("Monty Python.")
+
+# 2.1.1.14 Your very first program: The print() function - the keyword arguments
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+print("My name is", "Python.", end="\n")
+print("Monty Python.")
+
+
