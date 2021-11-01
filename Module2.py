c54082a4b19b345fb47862aa9cdede46f40ab051
David <51027303+wx2123@users.noreply.github.com>
2021-10-31 23:58:03 -0400

    Update Module2.py

--- Module2.py
+++ Module2.py
@@ -233,3 +233,16 @@ x = float(x)
 y = 3 * x ** 3 - 2 * x ** 2 + 3 * x -1
 # write your code here
 print("y =", y)
+
+# 2.5.1.2 LAB: Comments
+# this program computes the number of seconds in a given number of hours
+# this program has been written two days ago
+
+a = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", a) #printing the number of hours
+print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+print("Goodbye")
+#here we should also print "Goodbye", but a programmer didn't have time to write any code
+#this is the end of the program that computes the number of seconds in 3 hour
