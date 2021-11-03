a28c87cd89605d73dee0d817a078b82726f808b4
David <51027303+wx2123@users.noreply.github.com>
2021-11-03 15:39:24 -0400

    Update Module3.py

--- Module3.py
+++ Module3.py
@@ -81,3 +81,103 @@ largest_number = max(number1, number2, number3)
 # Print the result.
 print("The largest number is:", largest_number)
 
+
+# 3.1.1.10 LAB: Comparison operators and conditional execution
+x = input()
+if (x == 'Spathiphyllum'): print("Yes - Spathiphyllum is the best plant ever")
+elif (x == 'spathiphyllum'): print("No, I want a big Spathiphyllum!")
+else : print("Spathiphyllum! Not", x)
+
+# 3.1.1.11 LAB: Essentials of the if-else statement
+
+income = float(input("Enter the annual income: "))
+
+#
+# Write your code here.
+#
+
+if income <= 85528 : tax = (income) * .18 - 556.02
+else : tax = 14839 + (income - 85528) * .32
+
+tax = round(tax, 0)
+if (tax < 0) : tax = 0.0
+
+print("The tax is:", tax, "thalers")
+
+# 3.1.1.12 LAB: Essentials of the if-elif-else statement
+
+year = int(input("Enter a year: "))
+
+#
+# Write your code here.
+#	
+if year <= 1582 : print("Not within the Gregorian calendar period")
+elif year %   4 != 0: print("common year")
+elif year % 100 != 0: print('leap year')
+elif year % 400 != 0: print('common year')
+else : print ("leap year")
+
+
+# 3.1.1.14 SECTION SUMMARY (2/2)
+x = 1
+y = 1.0
+z = "1"
+
+if x == y:
+    print("one")
+if y == int(z):
+    print("two")
+elif x == y:
+    print("three")
+else:
+    print("four")
+
+
+
+# 3.2.1.1 Loops in Python | while
+# Store the current largest number here.
+largest_number = -999999999
+
+# Input the first value.
+number = int(input("Enter a number or type -1 to stop: "))
+
+# If the number is not equal to -1, continue.
+while number != -1:
+    # Is number larger than largest_number?
+    if number > largest_number:
+        # Yes, update largest_number.
+        largest_number = number
+    # Input the next number.
+    number = int(input("Enter a number or type -1 to stop: "))
+
+# Print the largest number.
+print("The largest number is:", largest_number)
+
+# 3.2.1.2 Loops in Python | while
+# A program that reads a sequence of numbers
+# and counts how many numbers are even and how many are odd.
+# The program terminates when zero is entered.
+
+odd_numbers = 0
+even_numbers = 0
+
+# Read the first number.
+number = int(input("Enter a number or type 0 to stop: "))
+
+# 0 terminates execution.
+while number != 0:
+    # Check if the number is odd.
+    if number % 2 == 1:
+        # Increase the odd_numbers counter.
+        odd_numbers += 1
+    else:
+        # Increase the even_numbers counter.
+        even_numbers += 1
+    # Read the next number.
+    number = int(input("Enter a number or type 0 to stop: "))
+
+# Print results.
+print("Odd numbers count:", odd_numbers)
+print("Even numbers count:", even_numbers)
+
+
