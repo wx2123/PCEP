bdee3f890842e6bfbd928936d4c6b3a27be32db6
David <51027303+wx2123@users.noreply.github.com>
2021-11-02 22:30:10 -0400

    11/2commit

---
+++ Module3.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  2 15:12:16 2021
+
+@author: Wujian
+"""
+# 3.1.1.2 Making decisions in Python: Equality: the equal to operator (==)
+var = 0  # Assigning 0 to var
+print(var == 0)
+
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+var = 0  # Assigning 0 to var
+print(var != 0)
+
+var = 1  # Assigning 1 to var
+print(var != 0)
+
+
+# 
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
+
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2: larger_number = number1
+else: larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
+
+# Read three numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+# We temporarily assume that the first number
+# is the largest one.
+# We will verify this soon.
+largest_number = number1
+
+# We check if the second number is larger than current largest_number
+# and update largest_number if needed.
+if number2 > largest_number:
+    largest_number = number2
+
+# We check if the third number is larger than current largest_number
+# and update largest_number if needed.
+if number3 > largest_number:
+    largest_number = number3
+
+# Print the result
+print("The largest number is:", largest_number)
+
+# 3.1.1.9 Making decisions in Python
+
+# Read three numbers.
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+# Check which one of the numbers is the greatest
+# and pass it to the largest_number variable.
+
+largest_number = max(number1, number2, number3)
+
+# Print the result.
+print("The largest number is:", largest_number)
+
---
+++ file_one.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  2 11:18:36 2021
+
+@author: Wujian
+
+https://www.freecodecamp.org/news/if-name-main-python-example/
+
+"""
+
+
+# Python file one module
+print("File one __name__ is set to: {}" .format(__name__))
+
+
+# Python module to import
+print("File two __name__ is set to: {}" .format(__name__))
