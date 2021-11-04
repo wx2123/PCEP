32a2edf83574ff48cd1aab88a6e028782f8ad890
David <51027303+wx2123@users.noreply.github.com>
2021-11-04 14:10:52 -0400

    Update Module3.py

--- Module3.py
+++ Module3.py
@@ -213,3 +213,16 @@ for expo in range(16):
     print("2 to the power of", expo, "is", power)
     power *= 2
 
+
+# 3.2.1.6 LAB: Essentials of the for loop - counting mississippily
+import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
+
+for i in range(1,6):
+    print(i,"Mississippi")
+    time.sleep(1)
