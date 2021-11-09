2db6d7df3209efb2d78d4e488bbe80bbae980c29
David <51027303+wx2123@users.noreply.github.com>
2021-11-09 16:27:35 -0500

    Update Module3.py

--- Module3.py
+++ Module3.py
@@ -724,3 +724,73 @@ for i in range(length // 2):
 print(my_list)
 
 
+# 3.4.1.13 LAB: The basics of lists - the Beatles
+# step 1
+beatles = []
+print("Step 1:", beatles)
+
+# step 2
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Step 2:", beatles)
+
+# step 3
+for i in ['Stu Sutcliffe', 'Pete Best']:
+    beatles.append(i)
+print("Step 3:", beatles)
+
+# step 4
+del beatles[3]
+del beatles[3]
+print("Step 4:", beatles)
+
+# step 5
+beatles.insert(0,"Ringo Starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
+
+
+# 3.5.1.2 Sorting simple lists - the bubble sort algorithm
+my_list = [8, 10, 6, 2, 4]  # list to sort
+for i in range(len(my_list) - 1):  # we need (5 - 1) comparisons
+    if my_list[i] > my_list[i + 1]:  # compare adjacent elements
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]  # If we end up here, we have to swap the elements.
+
+
+my_list = [8, 10, 6, 2, 4]  # list to sort
+swapped = True  # It's a little fake, we need it to enter the while loop.
+
+while swapped:
+    swapped = False  # no swaps so far
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True  # a swap occurred!
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print(my_list)
+
+
+# 3.5.1.3 Sorting simple lists - the bubble sort algorithm
+
+my_list = []
+swapped = True
+num = int(input("How many elements do you want to sort: "))
+
+for i in range(num):
+    val = float(input("Enter a list element: "))
+    my_list.append(val)
+
+while swapped:
+    swapped = False
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+print("\nSorted:")
+print(my_list)
+
+
