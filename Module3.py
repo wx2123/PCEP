cfb16afa16402cf823ac5d2e8f82b86299b24dc1
David <51027303+wx2123@users.noreply.github.com>
2021-11-09 19:59:44 -0500

    Update Module3.py

--- Module3.py
+++ Module3.py
@@ -793,4 +793,32 @@ while swapped:
 print("\nSorted:")
 print(my_list)
 
+# 3.6.1.1 Operations on lists
 
+list_1 = [1]
+list_2 = list_1
+list_1[0] = 2
+print(list_2) # need to remember!!!!
+
+
+# 3.6.1.2 Operations on lists | slices
+# Copying the entire list.
+list_1 = [1]
+list_2 = list_1[:]
+list_1[0] = 2
+print(list_2)
+
+# Copying some part of the list.
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:3]
+print(new_list)
+
+
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:-1]
+print(new_list)
+
+# 3.6.1.5 Operations on lists | slices, del
+my_list = [10, 8, 6, 4, 2]
+del my_list
+#print(my_list)
