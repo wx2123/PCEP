5347d9828b55956ec92cb9105b2af77dacd39b37
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-11-12 23:35:28 -0500

    Update Module4.py

--- Module4.py
+++ Module4.py
@@ -15,3 +15,19 @@ b = int(input())
 message()
 c = int(input())
 
+# 4.2.1.2 How functions communicate with their environment
+def message(number):
+    print("Enter a number:", number)
+
+number = 1234
+message(1)
+print(number)
+
+# 4.2.1.3 How functions communicate with their environment
+def message(what, number):
+    print("Enter", what, "number", number)
+
+# invoke the function
+message("telephone", 11)
+message("price", 5)
+message("number", "number")
