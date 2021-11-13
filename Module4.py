e1b3c23f23581fb0eb5797e9d9df48a605a59232
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-11-12 19:22:25 -0500

    Create Module4.py

---
+++ Module4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 17:32:33 2021
+
+@author: Wujian
+"""
+
+def message():
+    print("Enter a value: ")
+
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
+
