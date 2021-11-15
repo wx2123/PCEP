ec2094591f5c70ca39325e2356544103f8a2f3a6
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-11-14 23:46:45 -0500

    Update Module4.py

--- Module4.py
+++ Module4.py
@@ -149,3 +149,44 @@ for i in range(len(test_data)):
 		print("OK")
 	else:
 		print("Failed")
+    
+
+
+# 4.3.1.7 LAB: How many days: writing and using your own functions
+def is_year_leap(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+#
+# Your code from LAB 4.3.1.6.
+#
+
+def days_in_month(year, month):
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month==2:
+        if is_year_leap(year):
+            return 29
+        else:
+            return 28
+    elif month in [4,6,8,9,11]:
+        return 30
+    else:
+        return None
+#
+# Write your new code here.
+#
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+	yr = test_years[i]
+	mo = test_months[i]
+	print(yr, mo, "->", end="")
+	result = days_in_month(yr, mo)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")    
+    
+    
+    
