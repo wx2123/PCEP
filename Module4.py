3e5dad25b891992506cdd5baa1cb60e685c4f5f6
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-11-17 16:16:27 -0500

    Update Module4.py

--- Module4.py
+++ Module4.py
@@ -428,3 +428,40 @@ def area_of_triangle(a, b, c):
 
 print(area_of_triangle(1., 1., 2. ** .5))
 
+
+
+# 4.5.1.6 Creating functions | factorials
+def factorial_function(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    
+    product = 1
+    for i in range(2, n + 1):
+        product *= i
+    return product
+
+
+for n in range(1, 10):  # testing
+    print(n, factorial_function(n))
+
+# 4.5.1.7 Creating functions | Fibonacci numbers
+def fib(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+
+    elem_1 = elem_2 = 1
+    the_sum = 0
+    for i in range(3, n + 1):
+        the_sum = elem_1 + elem_2
+        elem_1, elem_2 = elem_2, the_sum
+    return the_sum
+
+
+for n in range(1, 10):  # testing
+    print(n, "->", fib(n))
+
+
