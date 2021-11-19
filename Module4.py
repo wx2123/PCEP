e14d24db03d8454a73579003d6d6ffb54ff20ee8
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-11-19 11:01:40 -0500

    Update Module4.py

--- Module4.py
+++ Module4.py
@@ -576,3 +576,52 @@ for english, french in dictionary.items():
 dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
 for french in dictionary.values():
     print(french)
+    
+    
+# 4.6.1.8 Tuples and dictionaries
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+dictionary['cat'] = 'minou'
+print(dictionary)
+
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+dictionary['swan'] = 'cygne'
+print(dictionary)
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+dictionary.update({"duck": "canard"})
+print(dictionary)
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+del dictionary['dog']
+print(dictionary)
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+dictionary.popitem()
+print(dictionary)    # outputs: {'cat': 'chat', 'dog': 'chien'}
+
+
+# 4.6.1.9 Tuples and dictionaries
+school_class = {}
+
+while True:
+    name = input("Enter the student's name: ")
+    if name == '':
+        break
+    
+    score = int(input("Enter the student's score (0-10): "))
+    if score not in range(0, 11):
+	    break
+    
+    if name in school_class:
+        school_class[name] += (score,)
+    else:
+        school_class[name] = (score,)
+        
+for name in sorted(school_class.keys()):
+    adding = 0
+    counter = 0
+    for score in school_class[name]:
+        adding += score
+        counter += 1
+    print(name, ":", adding / counter)
